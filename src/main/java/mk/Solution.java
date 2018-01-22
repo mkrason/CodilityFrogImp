e3a0c1a497be1d3aacea18a22a6ecf564a905c15
Marcin Krasoń <krasonm@phl.pharmalink.pl>
2018-01-22 23:31:24 +0100

    better performance

--- src/main/java/mk/Solution.java
+++ src/main/java/mk/Solution.java
@@ -12,4 +12,20 @@ public class Solution {
         return counter;
     }
 
+    public int solutionBeterPerformance(int x, int y, int d) {
+
+        int howLong = y - x;
+        int jumps = howLong / d;
+        int myResult = 0;
+
+        if (howLong == 0) {
+        } else if (howLong % d == 0) {
+            myResult = jumps;
+        } else {
+            myResult = jumps + 1;
+        }
+
+        return myResult;
+    }
+
 }
--- src/test/java/mk/SolutionTest.java
+++ src/test/java/mk/SolutionTest.java
@@ -18,6 +18,15 @@ class SolutionTest {
     }
 
     @Test
+    void checkArray11() {
+        int x = 10;
+        int y = 85;
+        int d = 30;
+        int result = solution.solutionBeterPerformance(x, y, d);
+        assertTrue(3 == result);
+    }
+
+    @Test
     void checkArray2() {
         int x = 1;
         int y = 1;
@@ -27,11 +36,29 @@ class SolutionTest {
     }
 
     @Test
+    void checkArray22() {
+        int x = 1;
+        int y = 1;
+        int d = 1;
+        int result = solution.solutionBeterPerformance(x, y, d);
+        assertTrue(0 == result);
+    }
+
+    @Test
     void checkArray3() {
         int x = 10;
         int y = 100;
         int d = 10;
         int result = solution.solution(x, y, d);
+        assertTrue(9 == result);
+    }
+
+    @Test
+    void checkArray33() {
+        int x = 10;
+        int y = 100;
+        int d = 10;
+        int result = solution.solutionBeterPerformance(x, y, d);
         assertTrue(9 == result);
     }
 
